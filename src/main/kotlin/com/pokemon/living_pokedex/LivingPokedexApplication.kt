c4bf0117492c5a9b8c4dfdb1ac47ee85c2315f33
MP-deving <marcospaulofm1@gmail.com>
2025-09-25 23:21:18 -0300

    config: setting dockerfile and docker-compose.yml

---
+++ src/main/kotlin/com/pokemon/living_pokedex/LivingPokedexApplication.kt
@@ -0,0 +1,11 @@
+package com.pokemon.living_pokedex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LivingPokedexApplication
+
+fun main(args: Array<String>) {
+	runApplication<LivingPokedexApplication>(*args)
+}
---
+++ src/test/kotlin/com/pokemon/living_pokedex/LivingPokedexApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pokemon.living_pokedex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LivingPokedexApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
